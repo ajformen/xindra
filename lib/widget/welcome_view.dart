3be719731e760d20d4a537aac62319da3b857718
Adeliso Formentera Jr <dev.ajformen@gmail.com>
2021-09-13 22:12:00 +0800

    added a drawer widget

--- lib/widget/welcome_view.dart
+++ lib/widget/welcome_view.dart
@@ -6,51 +6,60 @@ class WelcomeView extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.greenAccent),
+        backgroundColor: Colors.grey[800],
+        title: Text(
+          'Xindra',
+          style: TextStyle(
+            color: Colors.greenAccent,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+      ),
       backgroundColor: Colors.grey[850],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.greenAccent,
+              ),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: NetworkImage(controller.user.photoURL!),
+                radius: 30.0,
+              ),
+              accountName: Text(
+                '${controller.user.displayName}',
+                style: TextStyle(color: Colors.black),
+              ),
+              accountEmail: Text(
+                '${controller.user.email}',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                controller.logout();
+              },
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'User Profile',
+              'Products',
               style: TextStyle(fontSize: 30, color: Colors.greenAccent),
             ),
             SizedBox(
               height: 16,
-            ),
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: NetworkImage(controller.user.photoURL!),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Display Name : ${controller.user.displayName}',
-              style: TextStyle(fontSize: 18, color: Colors.white),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              controller.user.email!,
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints.tightFor(width: 120),
-              child: ElevatedButton(
-                child: Text(
-                  "Logout",
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-                onPressed: () {
-                  controller.logout();
-                },
-              ),
             ),
           ],
         ),
